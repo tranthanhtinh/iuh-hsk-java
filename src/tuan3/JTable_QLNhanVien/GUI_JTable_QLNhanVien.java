71ae931cfe83effc687d2c473b6cc14b338204a5
iamtun <letuan19431791.iuh@gmail.com>
2022-03-13 22:54:50 +0700

    update source code QLNhanVien - Staff model + ListStaff + saveFile + loadFile

--- src/tuan3/JTable_QLNhanVien/GUI_JTable_QLNhanVien.java
+++ src/tuan3/JTable_QLNhanVien/GUI_JTable_QLNhanVien.java
@@ -1,13 +1,11 @@
 package tuan3.JTable_QLNhanVien;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
